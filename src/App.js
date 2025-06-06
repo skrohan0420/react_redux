e0a6c76ea08eab663a39da3c31ab4b9fa7f69209
Rohan <skrohan0420@gmail.com>
2025-06-06 16:12:25 +0530

    added basic redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './features/counter/counterSlice';
 
 function App() {
+	const count = useSelector((state) => state.counter.value);
+	const dispatch = useDispatch();
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div style={{ textAlign: 'center', paddingTop: '2rem' }}>
+			<h1>Count: {count}</h1>
+			<button onClick={() => dispatch(increment())}>Increment</button>
+			<button onClick={() => dispatch(decrement())} style={{ marginLeft: '1rem' }}>
+				Decrement
+			</button>
+			<button
+				onClick={() => dispatch(incrementByAmount(5))}
+				style={{ marginLeft: '1rem' }}
+			>
+				Add 5
+			</button>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store'; // your Redux store
+import { Provider } from 'react-redux'; // <-- import Provider
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* <-- Wrap App with Provider */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
